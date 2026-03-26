6e36f429bdb43ad5c4650ab7778e3f648078fe7e
Oskar Wieczorek <oziw57@gmail.com>
2026-03-26 10:45:58 +0000

    add buttons

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 const btn = document.getElementById("btnHello");
 const output = document.getElementById("output");
 const statusEl = document.getElementById("status");
+const clickTimeTpl = document.getElementById("clickTimeTpl");
 
-statusEl.textContent = "Test";
+statusEl.textContent = "Ready";
 
 btn.addEventListener("click", () => {
-	output.textContent = `Button clicked at ${new Date().toLocaleTimeString()}`;
+	const now = new Date();
+
+	const fragment = clickTimeTpl.content.cloneNode(true);
+	const timeEl = fragment.querySelector(".click-time__value");
+
+	timeEl.textContent = now.toLocaleTimeString();
+	timeEl.setAttribute("datetime", now.toISOString());
+
+	output.appendChild(fragment);
 });
